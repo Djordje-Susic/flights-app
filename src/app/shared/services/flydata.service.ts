57099af31119fedb8154301dde9b98365d52dc7d
Djole <djordje.susic@gmail.com>
2022-04-25 18:02:59 +0200

    refactor: minor changes in FlydataService

--- src/app/shared/services/flydata.service.ts
+++ src/app/shared/services/flydata.service.ts
@@ -11,15 +11,18 @@ import { XmlParserService } from './xml-parser.service';
   providedIn: 'root'
 })
 export class FlydataService {
-  public serverUrl = environment.flyDataServer;
 
   constructor(
     private http: HttpClient,
     private xmlParserService: XmlParserService,
   ) { }
 
+  private gerUrl(airportCode: string): string{
+    return `${environment.flyDataServer}/XmlFeed.asp?TimeFrom=1&TimeTo=24&airport=${airportCode}&lastUpdate=${new Date().toISOString().substr(0,10)}`;
+  }
+
   getAirportFlightsData(airportCode: string){
-    return this.http.get(`${this.serverUrl}/XmlFeed.asp?TimeFrom=1&TimeTo=24&airport=${airportCode}`, {
+    return this.http.get(this.gerUrl(airportCode), {
       headers: new HttpHeaders().set('Accept', 'text/xml'),
       responseType: 'text'
     }).pipe(
@@ -38,7 +41,7 @@ export class FlydataService {
         subscriber.complete();
         worker.terminate();
       }
-      worker.postMessage(`${this.serverUrl}/XmlFeed.asp?TimeFrom=1&TimeTo=24&airport=${airportCode}`);
+      worker.postMessage(this.gerUrl(airportCode));
     }).pipe(
       concatMap(response => this.xmlParserService.parseXML(<string>response)),
       map(response => this.parseFlights(response)),
@@ -73,7 +76,7 @@ export class FlydataService {
     );
   }
 
-  parseFlights(data: any): Flight[] {
+  private parseFlights(data: any): Flight[] {
     const arr: Flight[] = [];
 
     const obj = data.airport.flights;
